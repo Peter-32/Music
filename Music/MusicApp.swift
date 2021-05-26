1cc4dce27091c01c3d4a01cd511b8d117c213045
Peter-32 <peter@impactradius.com>
2021-05-25 18:24:29 -0700

    init

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Peter Myers on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
