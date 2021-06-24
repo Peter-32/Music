da7852225121590ed3a1a86704089f0576f6dc11
Peter-32 <peter@impactradius.com>
2021-06-23 19:24:47 -0700

    create_buttons

--- Music/ContentView.swift
+++ Music/ContentView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
 struct Line: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -19,22 +25,152 @@ struct Line: Shape {
     }
 }
 
+struct Square: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        
+        return path
+                                                
+    }
+}
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        
+        return path
+                                                
+    }
+}
+
+
+func buttonA() {
+    
+}
+func buttonB() {
+    
+}
+func buttonC() {
+    
+}
+func buttonD() {
+    
+}
+func buttonE() {
+    
+}
+func buttonF() {
+    
+}
+func buttonG() {
+    
+}
+func buttonPlay() {
+    
+}
+func buttonUndo() {
+    
+}
+func buttonReset() {
+    
+}
 
 struct ContentView: View {
     var body: some View {
         return VStack() {
-            Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
-                                                            .round, lineJoin: .round)).frame(width: 300, height: 10)
-            Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
-                                                            .round, lineJoin: .round)).frame(width: 300, height: 10)
-            Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
-                                                            .round, lineJoin: .round)).frame(width: 300, height: 10)
-            Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
-                                                            .round, lineJoin: .round)).frame(width: 300, height: 10)
-            Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
-                                                            .round, lineJoin: .round)).frame(width: 300, height: 10)
+            VStack {
+                Arc(startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+            }
+            Spacer()
+            VStack {
+                Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
+                                                                .round, lineJoin: .round)).frame(width: UIScreen.screenWidth-32, height: 10)
+                Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
+                                                                .round, lineJoin: .round)).frame(width: UIScreen.screenWidth-32, height: 10)
+                Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
+                                                                .round, lineJoin: .round)).frame(width: UIScreen.screenWidth-32, height: 10)
+                Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
+                                                                .round, lineJoin: .round)).frame(width: UIScreen.screenWidth-32, height: 10)
+                Line().stroke(Color.black, style: StrokeStyle(lineWidth: 3, lineCap:
+                                                                .round, lineJoin: .round)).frame(width: UIScreen.screenWidth-32, height: 10)
+            }
+            Spacer()
+            HStack {
+                Button(action: buttonA) {
+                    Square()
+                        .fill(Color(red: 76/255, green: 162/255, blue: 255/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+                Button(action: buttonB) {
+                    Square()
+                        .fill(Color(red: 140/255, green: 81/255, blue: 165/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+                Button(action: buttonC) {
+                    Square()
+                        .fill(Color(red: 203/255, green: 94/255, blue: 152/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+                Button(action: buttonD) {
+                    Square()
+                        .fill(Color(red: 244/255, green: 123/255, blue: 138/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+            }
             
-                
+            HStack {
+                Button(action: buttonE) {
+                    Square()
+                        .fill(Color(red: 255/255, green: 163/255, blue: 127/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+                Button(action: buttonF) {
+                    Square()
+                        .fill(Color(red: 255/255, green: 210/255, blue: 133/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+                Button(action: buttonG) {
+                    Square()
+                        .fill(Color(red: 255/255, green: 255/255, blue: 165/255))
+                        .frame(width: UIScreen.screenWidth/7, height: 50)
+                }
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: buttonPlay) {
+                    Text("Play")
+                }
+                Spacer()
+                Button(action: buttonUndo) {
+                    Text("Undo")
+                }
+                Spacer()
+                Button(action: buttonReset) {
+                    Text("Reset")
+                }
+                Spacer()
+            }
+            Spacer()
         }
     }
 }
